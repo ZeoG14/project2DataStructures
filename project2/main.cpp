648d2d40ba4f06228d0d2b9d2b11aa2e9a46d7e4
Eliseo Garcia <zeog14@gmail.com>
2020-02-19 17:08:02 -0600

    Initial Commit

---
+++ project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project2
+//
+//  Created by Eliseo Garcia on 2/19/20.
+//  Copyright © 2020 Eliseo Garcia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
